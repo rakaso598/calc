c775daf6492ffe8cf4ca525a7f9626c9dbad0680
rakaso598 <nexonaddress98@gmail.com>
2025-01-31 09:48:50 +0900

    Add testSubtract

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function subtract(a, b) {
 function testAdd() {
   console.log(add(1,1) === 2);
 }
+
+function testSubtract() {
+  console.log(add(2,1) === 3);
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 # Calc
+
+## API
+
+- add(a, b)
+- subtract(a, b)
+
